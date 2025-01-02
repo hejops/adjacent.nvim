c63c7c8d91985286c7b2a5630e99e0926d5b7ab6
Joseph <hejops1@gmail.com>
2025-01-02 14:10:12 +0100

    feat: add exclude_binary option

--- lua/adjacent/init.lua
+++ lua/adjacent/init.lua
@@ -4,6 +4,7 @@ local M = {
 
 M.default_config = {
 	level = 1,
+	exclude_binary = false,
 }
 
 local builtin = require("telescope.builtin")
@@ -19,27 +20,20 @@ M.find = function()
 	local level = M.opts.level
 	local current_buffer_directory = vim.fn.expand("%:h")
 
+	local cmd = { "find", ".", "-maxdepth", tostring(level), "-type", "f" }
+
+	if M.opts.exclude_binary then
+		-- exclude executable binary files, but still include executable text files
+		-- https://unix.stackexchange.com/a/365705
+		for _, arg in pairs({ "-exec", "grep", "-I", "-q", ".", "{}", ";", "-print" }) do
+			table.insert(cmd, arg)
+		end
+	end
+
 	builtin.find_files({
 		prompt_title = "Adjacent",
 		cwd = current_buffer_directory,
-		find_command = {
-			"find",
-			".",
-			"-maxdepth",
-			tostring(level),
-			"-type",
-			"f",
-			-- exclude executable binary files, but still include executable text files
-			-- https://unix.stackexchange.com/a/365705
-			"-exec",
-			"grep",
-			"-I",
-			"-q",
-			".",
-			"{}",
-			";",
-			"-print",
-		},
+		find_command = cmd,
 	})
 end
 
